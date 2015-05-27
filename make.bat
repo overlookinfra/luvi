f7a3aab78a0e3fafae485030d775a5c61f866a31
Brian Maher <brian@brimworks.com>
2015-05-27 04:43:10 -0700

    Create separate 32bit vs 64bit targets, check errorlevel throughtout batch file.

--- make.bat
+++ make.bat
@@ -5,9 +5,16 @@ IF NOT "x%1" == "x" GOTO :%1
 
 GOTO :build
 
-:static
-ECHO "Building static"
+:static32
+ECHO "Building static 32"
 cmake -DWithOpenSSL=ON -DWithSharedOpenSSL=OFF -DWithZLIB=ON -DWithSharedZLIB=OFF -DWithSqlite=ON -DWithSharedSqlite=OFF -DWithCjson=ON -DWithYaml=ON -DWithSharedYaml=OFF -H. -Bbuild
+if %errorlevel% neq 0 exit /b %errorlevel%
+GOTO :end
+
+:static64
+ECHO "Building static 64"
+cmake -DWithOpenSSL=ON -DWithSharedOpenSSL=OFF -DWithZLIB=ON -DWithSharedZLIB=OFF -DWithSqlite=ON -DWithSharedSqlite=OFF -DWithCjson=ON -DWithYaml=ON -DWithSharedYaml=OFF -H. -Bbuild -G"Visual Studio 12 Win64"
+if %errorlevel% neq 0 exit /b %errorlevel%
 GOTO :end
 
 :tiny
@@ -16,9 +23,10 @@ cmake -T v120_xp -H. -Bbuild
 GOTO :end
 
 :build
-IF NOT EXIST build CALL Make.bat static
 cmake --build build --config Release -- /maxcpucount
+if %errorlevel% neq 0 exit /b %errorlevel%
 COPY build\Release\luvi.exe .
+if %errorlevel% neq 0 exit /b %errorlevel%
 GOTO :end
 
 :test
@@ -61,23 +69,49 @@ GOTO :end
 
 :reset
 git submodule update --init --recursive
+if %errorlevel% neq 0 exit /b %errorlevel%
 git clean -f -d
+if %errorlevel% neq 0 exit /b %errorlevel%
 git checkout .
+if %errorlevel% neq 0 exit /b %errorlevel%
 GOTO :end
 
-:publish
+:publish64
 CALL make.bat reset
-CALL make.bat static
+if %errorlevel% neq 0 exit /b %errorlevel%
+CALL make.bat static64
+if %errorlevel% neq 0 exit /b %errorlevel%
 CALL make.bat test
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 setlocal
 
 for /f %%i in ('git describe --tags "--abbrev=0"') do set VERSION=%%i
 
-set FNAME="luvi.Windows-%PROCESSOR_ARCHITECTURE%-%VERSION%.zip"
-7z a "%FNAME%" luvi.exe
+set FNAME="luvi.Windows-AMD64-%VERSION%.gz"
+7za a -tgzip "%FNAME%" luvi.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-aws --profile distelli-mvn-repo s3 cp "%FNAME%" "s3://distelli-mvn-repo/exe/Windows-%PROCESSOR_ARCHITECTURE%/%FNAME%"
+aws --profile distelli-mvn-repo s3 cp "%FNAME%" "s3://distelli-mvn-repo/exe/Windows-AMD64/%FNAME%"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
+:publish32
+CALL make.bat reset
+if %errorlevel% neq 0 exit /b %errorlevel%
+CALL make.bat static32
+if %errorlevel% neq 0 exit /b %errorlevel%
+CALL make.bat test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+setlocal
+
+for /f %%i in ('git describe --tags "--abbrev=0"') do set VERSION=%%i
+
+set FNAME="luvi.Windows-x86-%VERSION%.gz"
+7za a -tgzip "%FNAME%" luvi.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+aws --profile distelli-mvn-repo s3 cp "%FNAME%" "s3://distelli-mvn-repo/exe/Windows-x86/%FNAME%"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :end
