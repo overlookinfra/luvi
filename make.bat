2ff3dde702949e575282a6d05343be393349e9a0
Brian Maher <brian@brimworks.com>
2015-05-27 05:24:26 -0700

    Add GOTO :end that I forgot.

--- make.bat
+++ make.bat
@@ -83,17 +83,14 @@ CALL make.bat static64
 if %errorlevel% neq 0 exit /b %errorlevel%
 CALL make.bat test
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 setlocal
-
 for /f %%i in ('git describe --tags "--abbrev=0"') do set VERSION=%%i
-
 set FNAME="luvi.Windows-AMD64-%VERSION%.gz"
 7za a -tgzip "%FNAME%" luvi.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 aws --profile distelli-mvn-repo s3 cp "%FNAME%" "s3://distelli-mvn-repo/exe/Windows-AMD64/%FNAME%"
 if %errorlevel% neq 0 exit /b %errorlevel%
+GOTO :end
 
 :publish32
 CALL make.bat reset
@@ -102,16 +99,13 @@ CALL make.bat static32
 if %errorlevel% neq 0 exit /b %errorlevel%
 CALL make.bat test
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 setlocal
-
 for /f %%i in ('git describe --tags "--abbrev=0"') do set VERSION=%%i
-
 set FNAME="luvi.Windows-x86-%VERSION%.gz"
 7za a -tgzip "%FNAME%" luvi.exe
 if %errorlevel% neq 0 exit /b %errorlevel%
-
 aws --profile distelli-mvn-repo s3 cp "%FNAME%" "s3://distelli-mvn-repo/exe/Windows-x86/%FNAME%"
 if %errorlevel% neq 0 exit /b %errorlevel%
+GOTO :end
 
 :end
